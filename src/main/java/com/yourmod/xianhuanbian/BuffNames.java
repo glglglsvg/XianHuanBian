e03f1c79870ae6bb0d4de7ff6944fa96522304d3
glglglsvg <1978788037@qq.com>
2026-06-02 13:39:03 +0800

    Create BuffNames.java

---
+++ src/main/java/com/yourmod/xianhuanbian/BuffNames.java
@@ -0,0 +1,8 @@
+package com.yourmod.xianhuanbian;
+public class BuffNames {
+    public static final String[] NAME = {
+        "","壹·生息","贰·神力","叁·破界","肆·疾风","伍·浮游",
+        "陆·影弑","柒·穿刺","捌·赠予","玖·灾祸","拾·炼化",
+        "拾壹·仙变","拾贰·不灭"
+    };
+}
