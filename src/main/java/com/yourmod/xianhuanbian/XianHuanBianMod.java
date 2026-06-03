bf97be3c5df0994e022b6415096049c8818f8fe0
glglglsvg <1978788037@qq.com>
2026-06-03 23:14:18 +0800

    Update XianHuanBianMod.java

--- src/main/java/com/yourmod/xianhuanbian/XianHuanBianMod.java
+++ src/main/java/com/yourmod/xianhuanbian/XianHuanBianMod.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -15,6 +16,7 @@ public class XianHuanBianMod implements ModInitializer {
     public static final String MODID = "xianhuanbian";
     public static final Identifier UNLOCK_FIRST = new Identifier(MODID, "unlock_first");
     public static final Identifier TOGGLE_ALL = new Identifier(MODID, "toggle_all");
+    public static final Identifier SYNC_BUFFS = new Identifier(MODID, "sync_buffs");
 
     @Override
     public void onInitialize() {
@@ -24,77 +26,80 @@ public class XianHuanBianMod implements ModInitializer {
 
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-                PlayerBuffComponent data = ModComponents.BUFF_DATA.get(player);
-                if (data != null) {
-                    BuffEventHandler.applyActiveBuffs(player, data);
+                PlayerBuffData data = PlayerBuffData.get(player);
+                BuffEventHandler.applyActiveBuffs(player, data);
+                data.save(player);
+                if (player.age % 100 == 0) {
+                    syncToClient(player, data);
                 }
             }
         });
 
         AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
             if (!world.isClient && player instanceof ServerPlayerEntity sp) {
-                PlayerBuffComponent data = ModComponents.BUFF_DATA.get(sp);
-                if (data != null) {
-                    BuffEventHandler.processActivity(sp, data, 0.002f);
-                    ModComponents.BUFF_DATA.sync(sp);
-                }
+                PlayerBuffData data = PlayerBuffData.get(sp);
+                BuffEventHandler.processActivity(sp, data, 0.002f);
+                data.save(sp);
+                syncToClient(sp, data);
             }
             return net.minecraft.util.ActionResult.PASS;
         });
 
         PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
             if (!world.isClient && player instanceof ServerPlayerEntity sp) {
-                PlayerBuffComponent data = ModComponents.BUFF_DATA.get(sp);
-                if (data != null) {
-                    BuffEventHandler.processActivity(sp, data, 0.001f);
-                    ModComponents.BUFF_DATA.sync(sp);
-                }
+                PlayerBuffData data = PlayerBuffData.get(sp);
+                BuffEventHandler.processActivity(sp, data, 0.001f);
+                data.save(sp);
+                syncToClient(sp, data);
             }
         });
 
         ServerLivingEntityEvents.AFTER_DEATH.register((entity, source) -> {
             if (source.getAttacker() instanceof ServerPlayerEntity sp) {
-                PlayerBuffComponent data = ModComponents.BUFF_DATA.get(sp);
-                if (data != null) {
-                    BuffEventHandler.processActivity(sp, data, 0.003f);
-                    BuffEventHandler.handleKillExperience(sp, data, entity);
-                    ModComponents.BUFF_DATA.sync(sp);
-                }
+                PlayerBuffData data = PlayerBuffData.get(sp);
+                BuffEventHandler.processActivity(sp, data, 0.003f);
+                BuffEventHandler.handleKillExperience(sp, data, entity);
+                data.save(sp);
+                syncToClient(sp, data);
             }
         });
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            PlayerBuffComponent data = ModComponents.BUFF_DATA.get(handler.player);
-            if (data != null) ModComponents.BUFF_DATA.sync(handler.player);
+            PlayerBuffData data = PlayerBuffData.get(handler.player);
+            syncToClient(handler.player, data);
         });
 
         ServerPlayNetworking.registerGlobalReceiver(UNLOCK_FIRST, (server, player, handler, buf, responseSender) -> {
             server.execute(() -> {
-                PlayerBuffComponent data = ModComponents.BUFF_DATA.get(player);
-                if (data != null) {
-                    boolean hasAny = false;
-                    for (int i = 1; i <= 10; i++) {
-                        if (data.isUnlocked(i)) { hasAny = true; break; }
-                    }
-                    if (!hasAny) {
-                        int rand = player.getRandom().nextInt(10) + 1;
-                        data.setUnlocked(rand, true);
-                        data.setActive(rand, true);
-                        player.sendMessage(net.minecraft.text.Text.literal("你顿悟了" + BuffNames.NAME[rand] + "之力！"), false);
-                        ModComponents.BUFF_DATA.sync(player);
-                    }
+                PlayerBuffData data = PlayerBuffData.get(player);
+                boolean hasAny = false;
+                for (int i = 1; i <= 10; i++) {
+                    if (data.isUnlocked(i)) { hasAny = true; break; }
+                }
+                if (!hasAny) {
+                    int rand = player.getRandom().nextInt(10) + 1;
+                    data.setUnlocked(rand, true);
+                    data.setActive(rand, true);
+                    player.sendMessage(net.minecraft.text.Text.literal("你顿悟了" + BuffNames.NAME[rand] + "之力！"), false);
+                    data.save(player);
+                    syncToClient(player, data);
                 }
             });
         });
 
         ServerPlayNetworking.registerGlobalReceiver(TOGGLE_ALL, (server, player, handler, buf, responseSender) -> {
             server.execute(() -> {
-                PlayerBuffComponent data = ModComponents.BUFF_DATA.get(player);
-                if (data != null) {
-                    BuffEventHandler.toggleAllBuffs(player, data);
-                    ModComponents.BUFF_DATA.sync(player);
-                }
+                PlayerBuffData data = PlayerBuffData.get(player);
+                BuffEventHandler.toggleAllBuffs(player, data);
+                data.save(player);
+                syncToClient(player, data);
             });
         });
+    }
+
+    private static void syncToClient(ServerPlayerEntity player, PlayerBuffData data) {
+        var buf = PacketByteBufs.create();
+        buf.writeNbt(data.toNbt());
+        ServerPlayNetworking.send(player, SYNC_BUFFS, buf);
     }
 }
